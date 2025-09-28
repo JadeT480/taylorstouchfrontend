80bcbda1ecfb295de8b9df2700f292665ded8a02
Jade <jadet480@googlemail.com>
2025-09-28 14:43:40 +0100

    Set up React project structure with placeholder components

---
+++ taylors-touch/src/App.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+import Home from "./pages/Home";
+
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <Header />
+      <Home />
+    </div>
+  );
+}
---
+++ taylors-touch/src/components/Header.jsx
@@ -0,0 +1,3 @@
+export default function Header() {
+  return <header><h2>Taylor's Touch</h2></header>;
+}
