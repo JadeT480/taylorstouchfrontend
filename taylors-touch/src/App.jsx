f8ab2a64a9925a8d5199aecbed883f88ed2e488b
Jade <jadet480@googlemail.com>
2025-10-29 14:43:38 +0000

    Enable third party login

--- taylors-touch/src/App.jsx
+++ taylors-touch/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
@@ -5,8 +6,27 @@ import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/auth/user", { credentials: "include" }).then(res => {
+      if (res.ok) return res.json();
+      throw new Error("Not authenicated");
+    }).then(data => setUser(data)).catch(err => console.log(err));
+  }, []);
+
   return (
     <>
+      <div>
+        {user ? (
+          <div>
+            <h1>Welcome, {user.name}</h1>
+            <a href="http://localhost:3000/auth/logout">Logout</a>
+          </div>
+        ) : (
+          <a href="http://localhost:3000/auth/google">Login with Google</a>
+        )}
+      </div>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
